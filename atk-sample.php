0531187124fcc5e0e6aaf80d369d817b502a0395
Alain Belair <belair.alain@gmail.com>
2018-01-19 11:41:17 -0500

    initial commit
    Use with multiple atk plugin within Wordpress.

--- atk-sample.php
+++ atk-sample.php
@@ -13,18 +13,24 @@ namespace atksample;
 use atkwp\controllers\ComponentController;
 use atkwp\helpers\Pathfinder;
 
-require 'vendor/autoload.php';
+
+$atk_plugin_name = "atkevent";
+$atk_plugin_loader = $atk_plugin_name."_loader";
+$atk_plugin = __NAMESPACE__."\\Plugin";
+
+
+$$atk_plugin_loader = require 'vendor/ibelar/atk-wordpress/plugin-autoload.php';
+$$atk_plugin_loader->setPluginLoader(plugin_dir_path(__FILE__));
+$$atk_plugin_loader->enableLoader();
 
 if (array_search(ABSPATH . 'wp-admin/includes/plugin.php', get_included_files()) === false) {
 	require_once ABSPATH . 'wp-admin/includes/plugin.php';
 }
 
-$atk_plugin_name = "atkevent";
-$atk_plugin = __NAMESPACE__."\\Plugin";
-
-
-$$atk_plugin_name = new  $atk_plugin($atk_plugin_name, new Pathfinder(plugin_dir_path(__FILE__)), new ComponentController());
+$$atk_plugin_name = new  $atk_plugin($atk_plugin_name, new Pathfinder(plugin_dir_path(__FILE__)), new ComponentController(), $$atk_plugin_loader);
 
 if (!is_null( $$atk_plugin_name)) {
 	$$atk_plugin_name->boot(__FILE__);
 }
+
+$$atk_plugin_loader->disableLoader();
